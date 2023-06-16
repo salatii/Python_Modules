bb1c9eedd64a6abde0c44b034d05a073635d6ed4
kh24 <katjana-1.herbst@uni-ulm.de>
2023-06-16 23:05:00 +0200

    fixes summary.py

--- python_scripts/summary.py
+++ python_scripts/summary.py
@@ -1,7 +1,6 @@
 from transformers import pipeline
 import sys
 
-#Todo more Parameter for diffrent length -> SMS, E-Mail, Homepage
+content = sys.argv[1] #content for summary in english
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-content = sys.argv[1] #content for keywords in english
 print(summarizer(content, max_length=130, min_length=30, do_sample=False))
